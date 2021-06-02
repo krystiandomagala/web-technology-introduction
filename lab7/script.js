e8919fe254977461f45c027ab5205d54c7f0a53c
krystiandomagala <79994186+krystiandomagala@users.noreply.github.com>
2021-06-02 16:42:42 +0200

    gallery

--- lab7/script.js
+++ lab7/script.js
@@ -1,12 +1,13 @@
-var galleryImg = document.querySelectorAll(".gallery-item");
+const imgItem = document.querySelectorAll('.lightbox-item');
 
-galleryImg.forEach(item => {
-    item.addEventListener("click", () => {
-        var clone = item.cloneNode(true);
-        clone.style.position = "absolute";
-        console.log(this.getComputedStyle(item).getPropertyValue("height"));
-        clone.style.top = "calc(50vh - " + this.getComputedStyle(item).getPropertyValue('height') + "/2)";
-        clone.style.boxShadow = "100% 100% 30px #000";
-        document.body.appendChild(clone);
-    }); 
-});
+imgItem.forEach(item => {
+    item.addEventListener("click",openLightbox)
+})
+
+function openLightbox(){
+
+    const inLightbox = document.querySelector(".lightbox-img");
+    const lightboxContainer = document.querySelector(".lightbox-container");
+    inLightbox.src = this.src;
+    lightboxContainer.classList.add("lightbox-active");
+}
